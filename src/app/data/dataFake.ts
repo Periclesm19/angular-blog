f96c770f17d636a10bb94c6a7433af5e3fb5b130
Pericles Matos <spfcpericles@gmail.com>
2023-08-17 16:25:01 -0300

    content criate

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    id: '1',
+    photoCover:
+      'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg',
+    title: 'Novo filme Homem de Ferr',
+    description: 'Marvel anuncia novo filme do Homem de Ferro em 2024.',
+  },
+  {
+    id: '2',
+    photoCover:
+      'https://legadodamarvel.com.br/wp-content/uploads/2022/06/Marvel-vs.-DC-Qual-universo-de-herois-e-mais-poderoso-legadodamarvel.webp',
+    title: 'Liga da Justiça vs Vingadores',
+    description: '',
+  },
+  {
+    id: '3',
+    photoCover:
+      'https://www.setedias.com.br/sig/www/openged/conteudos/26394/026394_618d9620dded4_Duna_filme.jpg',
+    title: 'Filme Duna é vencedor do Oscar',
+    description: '',
+  },
+  {
+    id: '4',
+    photoCover:
+      'https://s2.glbimg.com/lip1rXjb7IfKWKhzwjKCCsKJcrA=/e.glbimg.com/og/ed/f/original/2017/08/17/game-of-thrones.jpg',
+    title: 'Game of Thrones tera um filme',
+    description: '',
+  },
+];
